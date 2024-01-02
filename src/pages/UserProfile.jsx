1e492f2d1e1b7228e69eba6f5c50012ecc789d78
Zach D'Amico <zachdamico90@gmail.com>
2024-01-02 17:15:32 -0600

    wip styling profile

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -28,20 +28,40 @@ export const UserProfile = ({ currentUser }) => {
   }, [currentUser]);
 
   return (
-    <div>
-      <h2>Welcome to Your Profile</h2>
+    <div className="__profile-background__ min-h-screen bg-black">
+      {/* <h2>Welcome to Your Profile</h2> */}
       <div>
         {user ? (
-          <section>
-            <div key="info-1">Profile Pic placeholder</div>
-            <div key="info-2">First Name: {user.first_name}</div>
-            <div key="info-3">Last Name: {user.last_name}</div>
-            <div key="info-4">Username: {user.username}</div>
-            <div key="info-5">Email: {user.email}</div>
-            <h3>Favorites: </h3>
-            {userFavorites.map((favorite, idx) => (
-              <div key={idx}>{favorite.restaurant.name}</div>
-            ))}
+          <section className="__profile-container__">
+            <div className="__profile-details__ bg-black flex flex-col items-center">
+              <div key="info-1" className="__profile-pic__ text-white">
+                Profile Pic placeholder
+              </div>
+              {/* <div key="info-2">First Name: {user.first_name}</div> */}
+              {/* <div key="info-3">Last Name: {user.last_name}</div> */}
+              <div key="info-4" className="__username__ text-white">
+                Username: {user.username}
+              </div>
+              <div key="info-5" className="__email__ text-white">
+                Email: {user.email}
+              </div>
+            </div>
+            <h3 className="__favorites-header__ text-red-700 bg-black text-3xl flex justify-center mt-8 mb-2">
+              Favorites:{" "}
+            </h3>
+            <div className="__img-grid__ grid grid-cols-3 gap-8">
+              {userFavorites.map((favorite, idx) => (
+                <div
+                  key={idx}
+                  className="__img-container__ flex justify-center flex-row"
+                >
+                  <img
+                    src={favorite.restaurant.img_url}
+                    className="__restaurant-img__ w-[200px] h-auto"
+                  />
+                </div>
+              ))}
+            </div>
           </section>
         ) : (
           <div>Loading or Error Message</div>
